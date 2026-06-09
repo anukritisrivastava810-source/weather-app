fdfa1839085c7772bb613de627c33ffd5ffa0dd4
Anukriti Srivastava <anukritisrivastava810@gmail.com>
2026-06-10 00:59:46 +0530

    late

--- weather_app/lib/weather_screen.dart
+++ weather_app/lib/weather_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -24,12 +25,112 @@ class WeatherScreen extends StatelessWidget {
           // ),
         ],
       ),
-      body: Column(
-        children: [
-          //main Card
-          const Placeholder(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //main Card
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text('300°F', style: TextStyle(
+                            fontSize: 32,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          ),
+                          const SizedBox(height: 16,),
+                          Icon(Icons.cloud,
+                          size: 64,),
+                          const SizedBox(height: 16,),
+                          Text('Rain', style: TextStyle(fontSize: 20,),)
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+             const Text('Weather Forecast', style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+            const SizedBox(height: 16,),
+            const SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                  HourlyForecastItem(),
+                ],
+              ),
+            ),
+            //weather forecast cards
+            const SizedBox(height: 20,),
+            const Text('Additional Information',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class HourlyForecastItem extends StatelessWidget {
+  const HourlyForecastItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Card(
+                      elevation: 6,
+                      child: Container(
+                        width: 100,
+                        padding: const EdgeInsets.all(8.0),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Column(
+                          children: [
+                            Text('03:00',
+                             style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            ),
+                            const SizedBox(height: 8),
+                            Icon(Icons.cloud,
+                             size: 32,
+                             ),
+                              SizedBox(height: 8,),
+                             Text(
+                              '320.12',
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
     );
   }
 }
